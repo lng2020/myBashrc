56a0c4c55d445b089e751f498b3a3c03dfe38fe4
lng2020 <nanguanlin6@gmail.com>
2023-01-29 00:08:01 +0800

    my init bash

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#Configure Git
+echo "Input your Git user.name"
+read GIT_USER_NAME
+echo "Input your Git user.email"
+read GIT_USER_EMAIL
+
+sudo apt update 
+
+git config --global user.name $GIT_USER_NAME
+git config --global user.email $GIT_USER_EMAIL
+
+#Install Java
+sudo apt install default-jdk default-jdk-doc default-jre default-jre-headless -y &&
+update-alternatives --display java
+
+#Install Mysql server and client
+sudo apt install -y mysql-server
+
+#Install nodejs
+sudo apt install -y nodejs npm
+
+#Install utilities 
+sudo apt install -y tmux
+sudo apt install -y vim
